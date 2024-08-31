053408058d4a2b58c9cb84d447b7c8d0f58f71ba
David Campos <david.castro001@edu.pucrs.br>
2024-08-31 06:36:31 -0300

    onboard changes

--- src/app/onboard/firstOnboard.tsx
+++ src/app/onboard/firstOnboard.tsx
@@ -1,21 +1,19 @@
 import { Button } from "@/components/ui/button";
 import "animate.css";
+import Image from "next/image";
 
 export default function FirstOnboard() {
   return (
     <div className="flex flex-col w-full h-full items-center justify-center text-center overflow-hidden">
-      <div className="animate__animated animate__slideInLeft bg-[#F4F4FF] border-l-[24px] border-b-[24x] border-[#F4F4FF] rounded-xl">
-        <div className="animate__animated animate__slideInLeft onBoardFrontContainerEnter flex flex-col items-center justify-center text-center overflow-hidden bg-[#FBFBFF] border rounded-xl w-[600px] h-[500px]">
+      <div className="animate__animated animate__slideInLeft w-3/4 h-3/4 bg-[#F4F4FF] border-l-[20px] border-b-[20px] border-[#F4F4FF] rounded-xl">
+        <div className="animate__animated animate__slideInLeft onBoardFrontContainerEnter flex flex-col items-center justify-between text-center overflow-hidden bg-[#FBFBFF] border rounded-xl w-full h-full p-8">
           <h1 className="animate__animated animate__fadeIn animate__delay-1s font-bold text-xl pb-4">
             Alerta Amigo
           </h1>
-          <img
-            className="animate__animated animate__fadeIn animate__delay-2s w-[320px] h-[340]"
-            src="/bot_gif.gif"
-          />
+          <Image className="animate__animated animate__fadeIn animate__delay-2s" src="/bot_gif.gif" alt="bot gif" width={250} height={250} />
 
           <div className="items-center px-8">
-            <p className="animate__animated animate__fadeIn animate__delay-3s text-balance text-center">
+            <p className="animate__animated animate__fadeIn animate__delay-3s text-justify text-xs pb-2">
               Nosso chatbot é treinado para fornecer informações e orientações
               precisas durante desastres. Ele está sempre pronto para te ajudar,
               seja para encontrar abrigo, obter primeiros socorros ou entender o
@@ -28,12 +26,6 @@ export default function FirstOnboard() {
         Próximo
       </Button>
 
-      <Button
-        variant={"ghost"}
-        className="hover:text-primaryGray hover:bg-transparent active:text-textGray text-primaryGray absolute bottom-2 left-9 animate__animated animate__fadeIn animate__delay-5s"
-      >
-        Skip
-      </Button>
     </div>
   );
 }
