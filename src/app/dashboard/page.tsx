e52d228c19458939a30f6319fa1a513d26a0f0a6
João Vicente Campanher <jvcampanher@gmail.com>
2024-08-31 07:56:39 -0300

    Tabela e integração

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,13 +1,27 @@
 "use client";
+import ImportIcon from "@/assets/ImportIcon";
+import TrashIcon from "@/assets/TrashIcon";
 import ChevronDown from "@/components/ChevronDown";
 import DropdownContextMenu from "@/components/DropdownContextMenu";
+import { Button } from "@/components/ui/button";
+import CategoriesBox from "@/components/ui/categoriesBox";
 import { useRouter } from "next/navigation";
+import { Fragment } from "react";
 
+const fileMocks = [
+  { name: "Guia de Incêndio" },
+  { name: "Guia de Incêndio" },
+  { name: "Guia de Incêndio" },
+  { name: "Guia de Incêndio" },
+  { name: "Guia de Incêndio" },
+  { name: "Guia de Incêndio" },
+  { name: "Guia de Incêndio" },
+];
 export default function Dashboard() {
   const router = useRouter();
 
   return (
-    <div className="h-full w-full overflow-y-auto px-6 pt-6">
+    <div className="h-full w-full overflow-y-auto p-6">
       <div className="justify-between flex">
         <span className="font-semibold text-4xl">Dashboard</span>
         <DropdownContextMenu
@@ -29,9 +43,40 @@ export default function Dashboard() {
             Categorias Disponíveis
           </div>
         </div>
-        <div className="flex gap-8 overflow-x-auto">
+        <div className="flex gap-4 overflow-x-auto">
           {[1, 2, 3, 4, 5, 6, 7, 8, 9].map((i) => (
-            <div key={i} className="min-w-44 h-44 bg-red-200 rounded-xl"></div>
+            <CategoriesBox
+              bgColor="#ff0000"
+              icon="🔥"
+              iconBgColor="#aa0000"
+              quantity={2}
+              title=""
+            />
+          ))}
+        </div>
+      </div>
+      <div className="shadow-lg rounded-3xl p-8 flex flex-col gap-4">
+        <div className="flex items-center gap-4">
+          <div className="font-semibold text-lg">Arquivos - Incêndio</div>
+          <Button variant="outline">
+            <ImportIcon />
+            Importar
+          </Button>
+        </div>
+        <div className="grid grid-cols-[1fr_10fr_1fr]">
+          <div className="text-gray-400 py-3">#</div>
+          <div className="text-gray-400 py-3">Nome</div>
+          <div className="text-gray-400 text-center py-3">Deletar</div>
+          {fileMocks.map((file, i) => (
+            <Fragment>
+              <div className="border-t border-gray-300 py-3">
+                {i.toString().padStart(2, "0")}
+              </div>
+              <div className="border-t border-gray-300 py-3">{file.name}</div>
+              <div className="flex justify-center border-t border-gray-300 py-3">
+                <TrashIcon />
+              </div>
+            </Fragment>
           ))}
         </div>
       </div>
--- src/components/ui/categoriesBox.tsx
+++ src/components/ui/categoriesBox.tsx
@@ -1,35 +1,38 @@
-import React from 'react';
+import React from "react";
 
-interface categoriesProps {
-  icon: string;  
+interface CategoriesPorps {
+  icon: string;
   title: string;
   quantity: number;
   iconBgColor: string;
   bgColor: string;
 }
 
-const CategoriesBox: React.FC<categoriesProps> = ({ icon, title, quantity, iconBgColor, bgColor }) => {
+const CategoriesBox: React.FC<CategoriesPorps> = ({
+  icon,
+  title,
+  quantity,
+  iconBgColor,
+  bgColor,
+}) => {
   return (
-    <div 
-      className={`flex flex-col items-start p-4 rounded-2xl transition-colors duration-300 w-44 h-44`}
+    <div
+      className="flex flex-col items-start p-4 rounded-2xl transition-colors duration-300 min-w-44 min-h-44"
       style={{ backgroundColor: bgColor }}
     >
-      {/* Icon area */}
       <div
         className="flex-shrink-0 flex items-center justify-center w-12 h-12 rounded-full mb-4"
         style={{ backgroundColor: iconBgColor }}
       >
         <span className="text-xl">{icon}</span>
       </div>
-      
-      {/* Title area */}
       <div className="mb-4">
         <h3 className="text-lg font-semibold">{title}</h3>
       </div>
-      
-      {/* Quantity area */}
       <div>
-        <p className="text-sm font-light text-gray-700">{quantity + " arquivos"}</p>
+        <p className="text-sm font-light text-gray-700">
+          {quantity + " arquivo" + (quantity > 1 ? "s" : "")}
+        </p>
       </div>
     </div>
   );
