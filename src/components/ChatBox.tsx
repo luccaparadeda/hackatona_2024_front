06b3b5ee3e3b3a5f4a1a57a9f077c9209da7b22f
João Vicente Campanher <jvcampanher@gmail.com>
2024-08-31 05:43:08 -0300

    remove bg red

--- src/components/ChatBox.tsx
+++ src/components/ChatBox.tsx
@@ -11,7 +11,7 @@ export default function ChatBox({
 }) {
   return (
     <div
-      className={`flex w-4/5 bg-red-500 rounded-md p-1 text-xs
+      className={`flex w-4/5 rounded-md p-1 text-xs
         ${
           fromUser
             ? "ml-auto bg-chatTextGray "
