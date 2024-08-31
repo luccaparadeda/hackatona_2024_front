b7d791340523434f263efbdb72cf4a9dc1753ffe
João Vicente Campanher <jvcampanher@gmail.com>
2024-08-31 15:04:55 -0300

    mocks certos

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -10,18 +10,29 @@ import api from "@/utils/api";
 import { Fragment, useEffect, useState } from "react";
 import { Category, getCategoryFrontInfo } from "../chatoptions/mocksContent";
 
-const fileMocks = [
-  { name: "Guia de Incêndio" },
-  { name: "Guia de Incêndio" },
-  { name: "Guia de Incêndio" },
-  { name: "Guia de Incêndio" },
-  { name: "Guia de Incêndio" },
-  { name: "Guia de Incêndio" },
-  { name: "Guia de Incêndio" },
-];
+function getFiles(name: string) {
+  if (name === "incendio") {
+    return [
+      { name: "Guia de Incêndio 1" },
+      { name: "Guia de Incêndio 2" },
+      { name: "Guia de Incêndio 3" },
+      { name: "Guia de Incêndio 4" },
+    ];
+  } else {
+    return [
+      { name: "Guia de Chuvas Intensas 1" },
+      { name: "Guia de Chuvas Intensas 2" },
+      { name: "Guia de Chuvas Intensas 3" },
+    ];
+  }
+}
 
 export default function Dashboard() {
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [selectedCategory, setSelectedCategory] = useState<Category | null>(
+    null,
+  );
+
   const { signOut } = useAuth();
 
   const [categoriesList, setCategoriesList] = useState<Category[]>([]);
@@ -41,18 +52,14 @@ export default function Dashboard() {
   async function getCategories() {
     const response = await api.get("/category");
     localStorage.setItem("categoriesDashboard", JSON.stringify(response.data));
-    setCategoriesList(
-      response.data.map(
-        (category: {
-          name: string;
-          description: string;
-          prompts: string[];
-        }) => ({
-          ...category,
-          ...getCategoryFrontInfo(category.name),
-        }),
-      ),
+    const categories = response.data.map(
+      (category: { name: string; description: string; prompts: string[] }) => ({
+        ...category,
+        ...getCategoryFrontInfo(category.name),
+      }),
     );
+    setSelectedCategory(categories[0]);
+    setCategoriesList(categories);
   }
 
   useEffect(() => {
@@ -84,42 +91,46 @@ export default function Dashboard() {
           {categoriesList.map((item, index) => (
             <CategoryBox
               className="cursor-pointer hover:"
-              onClick={() => console.log("clicked")}
+              onClick={() => setSelectedCategory(item)}
               key={index}
               iconBgColor={item.color}
               icon={item.emoji}
               bgColor={`${item.color}33`}
-              quantity={6 + index}
+              quantity={3 + index}
               title={item.exibitionName}
             />
           ))}
         </div>
       </div>
-      <div className="shadow-lg rounded-3xl px-8 pt-8 pb-2 flex flex-col gap-4">
-        <div className="flex items-center gap-4">
-          <div className="font-semibold text-lg">Arquivos - Incêndio</div>
-          <FileUpload />
+      {selectedCategory && (
+        <div className="shadow-lg rounded-3xl px-8 pt-8 pb-2 flex flex-col gap-4">
+          <div className="flex items-center gap-4">
+            <div className="font-semibold text-lg">
+              Arquivos - {selectedCategory.exibitionName}
+            </div>
+            <FileUpload />
+          </div>
+          <div className="grid grid-cols-[1fr_10fr_1fr]">
+            <div className="text-gray-400 py-3">#</div>
+            <div className="text-gray-400 py-3">Nome</div>
+            <div className="text-gray-400 text-center py-3">Deletar</div>
+            {getFiles(selectedCategory.name).map((file, i) => (
+              <Fragment key={i}>
+                <div className="border-t border-gray-300 py-3">
+                  {(i + 1).toString().padStart(2, "0")}
+                </div>
+                <div className="border-t border-gray-300 py-3">{file.name}</div>
+                <div
+                  className="flex justify-center border-t border-gray-300 py-3 cursor-pointer"
+                  onClick={() => handleDeleteClick()}
+                >
+                  <TrashIcon />
+                </div>
+              </Fragment>
+            ))}
+          </div>
         </div>
-        <div className="grid grid-cols-[1fr_10fr_1fr]">
-          <div className="text-gray-400 py-3">#</div>
-          <div className="text-gray-400 py-3">Nome</div>
-          <div className="text-gray-400 text-center py-3">Deletar</div>
-          {fileMocks.map((file, i) => (
-            <Fragment key={i}>
-              <div className="border-t border-gray-300 py-3">
-                {i.toString().padStart(2, "0")}
-              </div>
-              <div className="border-t border-gray-300 py-3">{file.name}</div>
-              <div
-                className="flex justify-center border-t border-gray-300 py-3 cursor-pointer"
-                onClick={() => handleDeleteClick()}
-              >
-                <TrashIcon />
-              </div>
-            </Fragment>
-          ))}
-        </div>
-      </div>
+      )}
       <Modal
         isOpen={isModalOpen}
         onClose={() => setIsModalOpen(false)}
